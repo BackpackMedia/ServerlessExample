8eb7464f6c427ac3e36b6929e301d588347959c5
Keheira <keheirah@gmail.com>
2019-12-05 18:07:57 -0500

    cleanup or something

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,51 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-
-//Default
+/*
+    Endpoint: /helloWorld
+*/
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Google Whack");
 });
 
-//C - Create
-// when user adds txt we push to the database
+/*
+    C - Create
+    when user adds txt we push to the database
+    Endpoint: /addComment?text=
+*/
 exports.addComment = functions.https.onRequest(async(request, response) => {
     message = request.query.text
-    snap = await admin.database().push(message)
-    response.redirect(303, snap.ref.toString())
+    snap = await admin.database().ref().push(message)
+    response.send(snap.ref.toString())
 })
-//R - Read
-//U - Update
-//D - Delete
 
+//R - Read
+//kinda useless for this but I'm going to write it anyway
 
 /*
-we just want all this to go to the db, we don't care about structure right now
-All data in key: value
-that being said the key won't be important on creation but does becaome important in RUD
+    U - Update
+    When user give a word we update it with another word
+    Endpoint: /updateComment?old=&new=
 */
+
+exports.updateComment = functions.https.onRequest(async(request, response) => {
+    //get query params
+    og_msg = request.query.old
+    new_msg = request.query.new
+    var key = ''
+    var path = admin.database().ref()
+    //search db for old
+    // eslint-disable-next-line promise/catch-or-return
+    // eslint-disable-next-line promise/always-return
+    path.once("value").then(snap => {
+        snap.forEach(child => {
+        if(child.val() === og_msg) //check to see if the value is equal to the og text
+            key = child.key
+        })
+    })
+    snap = await admin.database().ref().child(key).update(new_msg)
+    response.redirect(303, snap.ref.toString()) //this is ok for now so we can see change
+})
+//D - Delete
+//Allow user to delete a word
